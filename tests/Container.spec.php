69c3e41a08be04c9b89b7d0714d8f9a31d9ad645
pmall <pierre.mallinjoud@gmail.com>
2017-10-18 10:08:12 +0200

    use stubs in tests

--- tests/Container.spec.php
+++ tests/Container.spec.php
@@ -1,6 +1,7 @@
 <?php
 
 use function Eloquent\Phony\mock;
+use function Eloquent\Phony\stub;
 
 use Psr\Container\ContainerInterface;
 
@@ -61,11 +62,11 @@ describe('Container', function () {
             $this->provider1 = mock(ServiceProviderInterface::class);
             $this->provider2 = mock(ServiceProviderInterface::class);
             $this->provider3 = mock(ServiceProviderInterface::class);
-            $this->factory1 = mock(['__invoke' => function () {}]);
-            $this->factory2 = mock(['__invoke' => function () {}]);
-            $this->extension1 = mock(['__invoke' => function () {}]);
-            $this->extension2 = mock(['__invoke' => function () {}]);
-            $this->extension3 = mock(['__invoke' => function () {}]);
+            $this->factory1 = stub(function () {});
+            $this->factory2 = stub(function () {});
+            $this->extension1 = stub(function () {});
+            $this->extension2 = stub(function () {});
+            $this->extension3 = stub(function () {});
 
             $this->provider1->getFactories->returns([]);
             $this->provider1->getExtensions->returns([]);
@@ -78,65 +79,65 @@ describe('Container', function () {
 
         it('should run the factory associated with the given id', function () {
 
-            $this->provider1->getFactories->returns(['id' => $this->factory1->get()]);
+            $this->provider1->getFactories->returns(['id' => $this->factory1]);
 
             $container = new Container([$this->provider1->get()]);
 
-            $this->factory1->__invoke->with($container)->returns('service');
+            $this->factory1->with($container)->returns('service');
 
             $test = $container->get('id');
 
             expect($test)->toEqual('service');
-            $this->factory1->__invoke->called();
+            $this->factory1->called();
 
         });
 
         it('should run the last factory associated with the given id', function () {
 
-            $this->provider1->getFactories->returns(['id' => $this->factory1->get()]);
-            $this->provider2->getFactories->returns(['id' => $this->factory2->get()]);
+            $this->provider1->getFactories->returns(['id' => $this->factory1]);
+            $this->provider2->getFactories->returns(['id' => $this->factory2]);
 
             $container = new Container([
                 $this->provider1->get(),
                 $this->provider2->get(),
             ]);
 
-            $this->factory2->__invoke->with($container)->returns('service');
+            $this->factory2->with($container)->returns('service');
 
             $test = $container->get('id');
 
             expect($test)->toEqual('service');
-            $this->factory2->__invoke->called();
+            $this->factory2->called();
 
         });
 
         it('should run the extensions associated with the given id with null as parameter when no factory is defined', function () {
 
-            $this->provider1->getExtensions->returns(['id' => $this->extension1->get()]);
-            $this->provider2->getExtensions->returns(['id' => $this->extension2->get()]);
+            $this->provider1->getExtensions->returns(['id' => $this->extension1]);
+            $this->provider2->getExtensions->returns(['id' => $this->extension2]);
 
             $container = new Container([
                 $this->provider1->get(),
                 $this->provider2->get(),
             ]);
 
-            $this->extension1->__invoke->with($container, null)->returns('service1');
-            $this->extension2->__invoke->with($container, 'service1')->returns('service2');
+            $this->extension1->with($container, null)->returns('service1');
+            $this->extension2->with($container, 'service1')->returns('service2');
 
             $test = $container->get('id');
 
             expect($test)->toEqual('service2');
-            $this->extension1->__invoke->called();
-            $this->extension2->__invoke->called();
+            $this->extension1->called();
+            $this->extension2->called();
 
         });
 
         it('should run the extensions associated with the given id with the service built by the factory as parameter', function () {
 
-            $this->provider1->getFactories->returns(['id' => $this->factory1->get()]);
-            $this->provider1->getExtensions->returns(['id' => $this->extension1->get()]);
-            $this->provider2->getExtensions->returns(['id' => $this->extension2->get()]);
-            $this->provider3->getExtensions->returns(['id' => $this->extension3->get()]);
+            $this->provider1->getFactories->returns(['id' => $this->factory1]);
+            $this->provider1->getExtensions->returns(['id' => $this->extension1]);
+            $this->provider2->getExtensions->returns(['id' => $this->extension2]);
+            $this->provider3->getExtensions->returns(['id' => $this->extension3]);
 
             $container = new Container([
                 $this->provider1->get(),
@@ -144,27 +145,27 @@ describe('Container', function () {
                 $this->provider3->get(),
             ]);
 
-            $this->factory1->__invoke->with($container)->returns('service1');
-            $this->extension1->__invoke->with($container, 'service1')->returns('service2');
-            $this->extension2->__invoke->with($container, 'service2')->returns('service3');
-            $this->extension3->__invoke->with($container, 'service3')->returns('service4');
+            $this->factory1->with($container)->returns('service1');
+            $this->extension1->with($container, 'service1')->returns('service2');
+            $this->extension2->with($container, 'service2')->returns('service3');
+            $this->extension3->with($container, 'service3')->returns('service4');
 
             $test = $container->get('id');
 
             expect($test)->toEqual('service4');
-            $this->factory1->__invoke->called();
-            $this->extension1->__invoke->called();
-            $this->extension2->__invoke->called();
-            $this->extension3->__invoke->called();
+            $this->factory1->called();
+            $this->extension1->called();
+            $this->extension2->called();
+            $this->extension3->called();
 
         });
 
         it('should not care about the order of the service providers', function () {
 
-            $this->provider1->getFactories->returns(['id' => $this->factory1->get()]);
-            $this->provider1->getExtensions->returns(['id' => $this->extension1->get()]);
-            $this->provider2->getExtensions->returns(['id' => $this->extension2->get()]);
-            $this->provider3->getExtensions->returns(['id' => $this->extension3->get()]);
+            $this->provider1->getFactories->returns(['id' => $this->factory1]);
+            $this->provider1->getExtensions->returns(['id' => $this->extension1]);
+            $this->provider2->getExtensions->returns(['id' => $this->extension2]);
+            $this->provider3->getExtensions->returns(['id' => $this->extension3]);
 
             $container = new Container([
                 $this->provider2->get(),
@@ -172,18 +173,18 @@ describe('Container', function () {
                 $this->provider3->get(),
             ]);
 
-            $this->factory1->__invoke->with($container)->returns('service1');
-            $this->extension2->__invoke->with($container, 'service1')->returns('service2');
-            $this->extension1->__invoke->with($container, 'service2')->returns('service3');
-            $this->extension3->__invoke->with($container, 'service3')->returns('service4');
+            $this->factory1->with($container)->returns('service1');
+            $this->extension2->with($container, 'service1')->returns('service2');
+            $this->extension1->with($container, 'service2')->returns('service3');
+            $this->extension3->with($container, 'service3')->returns('service4');
 
             $test = $container->get('id');
 
             expect($test)->toEqual('service4');
-            $this->factory1->__invoke->called();
-            $this->extension1->__invoke->called();
-            $this->extension2->__invoke->called();
-            $this->extension3->__invoke->called();
+            $this->factory1->called();
+            $this->extension1->called();
+            $this->extension2->called();
+            $this->extension3->called();
 
         });
 

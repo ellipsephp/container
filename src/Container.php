26beff44ce27323ffdb48412c286804021274e4c
pmall <pierre.mallinjoud@gmail.com>
2018-03-04 21:18:38 +0100

    refactoring

--- src/Container.php
+++ src/Container.php
@@ -4,11 +4,26 @@ namespace Ellipse;
 
 use TypeError;
 
-use Ellipse\Container\AbstractContainer;
+use Psr\Container\ContainerInterface;
+use Psr\Container\ContainerExceptionInterface;
+
+use Interop\Container\ServiceProviderInterface;
+
+use Ellipse\Container\CachedServiceFactory;
+use Ellipse\Container\CompositeServiceFactory;
+use Ellipse\Container\Exceptions\ContainerException;
+use Ellipse\Container\Exceptions\NotFoundException;
 use Ellipse\Container\Exceptions\ServiceProviderTypeException;
 
-class Container extends AbstractContainer
+class Container implements ContainerInterface
 {
+    /**
+     * Associative array of alias => service factory pairs.
+     *
+     * @var array
+     */
+    private $factories = [];
+
     /**
      * Set up a container with the given array of service providers.
      *
@@ -19,9 +34,10 @@ class Container extends AbstractContainer
     {
         try {
 
-            $providers = array_values($providers);
-
-            parent::__construct(...$providers);
+            $this->factories = $this->merge(
+                $this->serviceFactoryMap($providers),
+                $this->serviceExtensionMap($providers)
+            );
 
         }
 
@@ -30,5 +46,98 @@ class Container extends AbstractContainer
             throw new ServiceProviderTypeException($providers, $e);
 
         }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function has($id)
+    {
+        return isset($this->factories[$id]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function get($id)
+    {
+        if ($this->has($id)) {
+
+            try {
+
+                return $this->factories[$id]($this);
+
+            }
+
+            catch (ContainerExceptionInterface $e) {
+
+                throw new ContainerException($id, $e);
+
+            }
+
+        }
+
+        throw new NotFoundException($id);
+    }
+
+    /**
+     * Return a service factory map from the given service providers.
+     *
+     * @param array $providers
+     * @return array
+     */
+    private function serviceFactoryMap(array $providers): array
+    {
+        $wrap = function ($factory) { return [$factory]; };
+
+        return array_reduce($providers, function ($factories, ServiceProviderInterface $provider) use ($wrap) {
+
+            return array_merge($factories, array_map($wrap, $provider->getFactories()));
+
+        }, []);
+    }
+
+    /**
+     * Return a service extension map from the given service providers.
+     *
+     * @param array $providers
+     * @return array
+     */
+    private function serviceExtensionMap(array $providers): array
+    {
+        $wrap = function ($factory) { return [$factory]; };
+
+        return array_reduce($providers, function ($extensions, ServiceProviderInterface $provider) use ($wrap) {
+
+            return array_merge_recursive($extensions, array_map($wrap, $provider->getExtensions()));
+
+        }, []);
+    }
+
+    /**
+     * Return the map resulting from merging the given service factory map and
+     * service extension map.
+     *
+     * @param array $factories
+     * @param array $extensions
+     * @return array
+     */
+    private function merge(array $factories, array $extensions): array
+    {
+        return array_map([$this, 'factory'], array_merge_recursive($factories, $extensions));
+    }
+
+    /**
+     * Return a cached composite service factory from the given array of service
+     * factories.
+     *
+     * @param array $factories
+     * @return \Ellipse\Container\CachedServiceFactory
+     */
+    private function factory(array $factories): CachedServiceFactory
+    {
+        return new CachedServiceFactory(
+            new CompositeServiceFactory($factories)
+        );
     }
 }
